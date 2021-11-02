52f5eeaee80e3754a7fd760d3e45b97a121bed96
chanbzz <cboswell1024@gmail.com>
2021-11-02 17:24:05 -0400

    authentication w breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WindowsThrowbackController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,4 +13,12 @@ use App\Http\Controllers\WindowsThrowbackController;
 |
 */
 
-Route::get('/', [WindowsThrowbackController::class, 'create']);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
